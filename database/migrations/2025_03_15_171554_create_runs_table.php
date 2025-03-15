032c4907e4de4d7f73e5b2d87315c02904424f6e
CandidoRPNeto <candido.neto@ucsal.edu.br>
2025-03-15 14:48:11 -0300

    add migrations

---
+++ database/migrations/2025_03_15_171554_create_runs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('runs', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('dealer_id');
+            $table->uuid('store_id');
+            $table->timestamp('finished_at')->nullable();
+            $table->timestamp('started_at')->nullable();
+            $table->timestamps();
+            $table->foreign('dealer_id')->references('id')->on('dealers');
+            $table->foreign('store_id')->references('id')->on('stores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('runs');
+    }
+};
---
+++ database/migrations/2025_03_15_171852_create_deliveries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('run_id');
+            $table->uuid('rating_id');
+            $table->uuid('destiny_id');
+            $table->string('code');
+            $table->integer('position')->default(0);
+            $table->timestamp('finished_at')->nullable();
+            $table->timestamps();
+            $table->foreign('run_id')->references('id')->on('runs');
+            $table->foreign('rating_id')->references('id')->on('ratings');
+            $table->foreign('destiny_id')->references('id')->on('locations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deliveries');
+    }
+};
